204b120dee0c5c0968cf894e24aa2e96769b13dd
journnie <155598826+journnie@users.noreply.github.com>
2024-01-23 11:45:09 +0900

    Time: 207 ms (97.91%), Space: 17.7 MB (59.63%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
@@ -2,29 +2,13 @@ class Solution:
     def checkXMatrix(self, grid: List[List[int]]) -> bool:
          # n x n matrix
         n = len(grid)
-
-        # check the diagonals != 0; True
-        # primary diagonal elements
-        row = 0
-        col = 0
-        while row < n:
-            if grid[row][col] == 0:
-                return False
-            row += 1
-            col += 1
-        # secondary diagonal elements
-        row = 0
-        col = n - 1
-        while row < n:
-            if grid[row][col] == 0:
-                return False
-            row += 1
-            col -= 1
-
-        # check the non-diagonals == 0; True
+         # check the non-diagonals == 0; True
         for i in range(n):
             for j in range(n):
-                if i != j and j != n-1-i:
+                if i == j or j == n-1-i:
+                    if grid[i][j] == 0:
+                        return False
+                else:
                     if grid[i][j] != 0:
                         return False
         return True
