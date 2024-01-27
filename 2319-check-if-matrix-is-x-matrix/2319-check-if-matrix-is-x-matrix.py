14e6c42d721c6ac45827b94b3fdc9d3eda8f1e09
journnie <155598826+journnie@users.noreply.github.com>
2024-01-28 00:06:58 +0900

    Time: 202 ms (99.53%), Space: 17.7 MB (59.53%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
@@ -1,14 +1,14 @@
 class Solution:
     def checkXMatrix(self, grid: List[List[int]]) -> bool:
-         # n x n matrix
-        n = len(grid)
-         # check the non-diagonals == 0; True
-        for i in range(n):
-            for j in range(n):
-                if i == j or j == n-1-i:
-                    if grid[i][j] == 0:
-                        return False
-                else:
-                    if grid[i][j] != 0:
-                        return False
+        for i in range(len(grid)):
+            # check if the green part has zero
+            if grid[i][i] == 0 or grid[i][len(grid)-1 - i] == 0:
+                return False
+            # if non-zeros, change them into zeros
+            grid[i][i] = 0
+            grid[i][len(grid) - 1 - i] = 0
+            # check if the whole row is a list of zero
+            if grid[i] != [0] * len(grid[0]):
+                return False
+
         return True
