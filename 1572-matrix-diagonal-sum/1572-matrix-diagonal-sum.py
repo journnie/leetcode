e025433163537ebbca872526c35d0660a5de699b
journnie <155598826+journnie@users.noreply.github.com>
2024-01-30 23:09:02 +0900

    Time: 101 ms (49.39%), Space: 16.8 MB (66.71%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -1,17 +1,18 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
+        # n x n square matrix
         n = len(mat)
-        primary_sum = 0
-        secondary_sum = 0
-        for i in range(n):
-            primary_sum += mat[i][i]
+        diag_sum = 0
 
-        for j in range(n):
-            print(j, n - 1 -j)
-            secondary_sum += mat[j][n - 1 -j]
+        if n == 1:
+            return mat[0][0]
+
+        for i in range(n):
+            diag_sum += mat[i][i] + mat[i][n-i-1]
+
         if n % 2 != 0:
-            mid = n//2
-            overlap = mat[mid][mid]
-        else:
-            overlap = 0
-        return primary_sum + secondary_sum - overlap
+            mid = n // 2
+            diag_sum -= mat[mid][mid]
+            
+
+        return diag_sum
