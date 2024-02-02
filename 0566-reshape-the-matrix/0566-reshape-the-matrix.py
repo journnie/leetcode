d519ca5be3e945f1b66a74d3252b247be8058a05
journnie <155598826+journnie@users.noreply.github.com>
2024-02-02 11:11:31 +0900

    Time: 73 ms (83.39%), Space: 17.6 MB (52.37%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.py
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -3,21 +3,20 @@ class Solution:
         # m x n matrix
         m = len(mat)
         n = len(mat[0])
+
         numbers = []
         reshaped_mat = [[0 for i in range(c)] for j in range(r)]
 
+        # the reshape operation with given parameters is not possible
+        if m * n != r * c:
+            return mat  # output the original matrix.
+        
         for row in mat:
             for element in row:
                 numbers.append(element)
 
-
-        # If the reshape operation with given parameters is possible and legal,
-        # output the new reshaped matrix;
-        if m * n == r * c:
-            for i in range(r):
-                for j in range(c):
-                    reshaped_mat[i][j] = numbers[(c * i) + j]
-            return reshaped_mat
-
-        # Otherwise, output the original matrix.
-        return mat
+        for i in range(r):
+            for j in range(c):
+                reshaped_mat[i][j] = numbers[(c * i) + j]
+                
+        return reshaped_mat
