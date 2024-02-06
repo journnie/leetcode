83345e8b57473a60a6e86f242022fa22dd2af21d
journnie <155598826+journnie@users.noreply.github.com>
2024-02-06 23:33:43 +0900

    Time: 107 ms (68.32%), Space: 17.1 MB (78.47%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,11 +1,11 @@
 class Solution:
-
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        sort_list = []
-        name_list = []
+        people_dict = {}
+
         for i in range(len(names)):
-            sort_list.append([heights[i], names[i]])
-        sort_list.sort(reverse=True)
-        for i in range(len(sort_list)):
-            name_list.append(sort_list[i][1])
-        return name_list
+            people_dict[heights[i]] = names[i]
+
+        sorted_dict = sorted(people_dict.items(), reverse=True)
+        sorted_names = [i[1] for i in sorted_dict]
+        
+        return sorted_names
