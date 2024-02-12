4c5a74fc4d3d98214aa5e2a9dadfb741e72a9823
journnie <155598826+journnie@users.noreply.github.com>
2024-02-12 16:56:40 +0900

    Time: 71 ms (92.66%), Space: 16.6 MB (81.93%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,9 +1,11 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        # check if the diagonally below element is the same
-        for i in range(len(matrix) - 1):
-            for j in range(len(matrix[0]) - 1):
-                number = matrix[i][j]
-                if number != matrix[i + 1][j + 1]:
+        # m x n matrix
+        m = len(matrix)
+        n = len(matrix[0])
+
+        for i in range(m-1):
+            for j in range(n-1):
+                if matrix[i][j] != matrix[i+1][j+1]:
                     return False
         return True
