7c9ef5f953c33fbbb6642064cd3652a122dcf920
journnie <155598826+journnie@users.noreply.github.com>
2024-02-12 14:42:34 +0900

    Time: 201 ms (51.44%), Space: 18.2 MB (64.89%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.py
+++ 0495-teemo-attacking/0495-teemo-attacking.py
@@ -1,18 +1,12 @@
 class Solution:
     def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
-        total_damage = 0
-        trials = len(timeSeries)
+        attacks = len(timeSeries)
+        poisoned = 0
+        for i in range(attacks):
+            if i < attacks-1 and timeSeries[i+1] - timeSeries[i] < duration:
+                poisoned += timeSeries[i+1] - timeSeries[i]
+            else:
+                poisoned += duration
 
-        for i in range(trials) :
-            if i < trials - 1 :
-                if timeSeries[i+1] - timeSeries[i] >= duration:
-                    total_damage += duration
-                
-                if timeSeries[i+1] - timeSeries[i] < duration:
-                    total_damage += timeSeries[i+1] - timeSeries[i]
-            
-            else : # i == trials
-                total_damage += duration
-
-        return total_damage
-            
+        return poisoned
+                        
