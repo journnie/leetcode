12b00f7b6aa31cc59d3b90faecaa61988215db25
journnie <155598826+journnie@users.noreply.github.com>
2024-02-13 23:46:13 +0900

    Time: 138 ms (84.74%), Space: 17.6 MB (32.34%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
@@ -3,18 +3,21 @@ class Solution:
         # m x n matrix
         m = len(mat)
         n = len(mat[0])
+
         location = []
         
-        special = 0
+        special = 0 
 
         rows = [0] * m
         cols = [0] * n
+
         for i in range(m):
             for j in range(n):
                 if mat[i][j] == 1:
                     location.append([i, j])
                     rows[i] += 1
                     cols[j] += 1
+                    
         for loc in location:
             x, y = loc
             if rows[x] == 1 and cols[y] == 1:
